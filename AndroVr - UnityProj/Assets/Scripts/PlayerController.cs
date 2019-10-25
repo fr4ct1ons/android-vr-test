38e382d4a328894bff729a0a24843c17149bcf37
Gabriel Lucena <gabriellucena23@gmail.com>
2019-10-25 15:25:54 -0300

    setup project

---
+++ AndroVr - UnityProj/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float speed;
+
+    private Rigidbody myRb;
+    private Vector3 bufferVector;
+
+    private void Awake()
+    {
+        myRb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bufferVector.Set(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+        Debug.Log("bufferVector: " + bufferVector);
+        bufferVector *= speed * Time.deltaTime;
+        transform.position = transform.position + bufferVector;
+    }
+}
